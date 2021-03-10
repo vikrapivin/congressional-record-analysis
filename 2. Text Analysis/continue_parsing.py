41508860f2b318ca237463205dc74c5036291cd5
Justin Schulberg <schulbergjustin@gmail.com>
2021-03-09 21:21:22 -0500

    Pandas Pandas Pandas
    Added some light code to read the json file in as a pandas dataframe

--- 2. Text Analysis/continue_parsing.py
+++ 2. Text Analysis/continue_parsing.py
@@ -1,3 +1,4 @@
+#%%
 # load previously generated json file for a particular CR date 
 import json
 import os
@@ -9,9 +10,10 @@ import numpy as np
 
 #%%
 dateString = 'CREC-2021-02-24'
-cr_json_filepath = 'json_ouput/'+dateString+'/cr.json'
-cr_json_file = open(cr_json_filepath, "r")
-cr_json = cr_json_file.read()
+cr_json_filepath = 'json_output/' + dateString + '/cr.json'
+
+with open('/Users/Owner/Documents/Blog/congressional-record-analysis/json_output/CREC-2021-02-24/cr.json', "r") as file:
+    cr_json = file.read()
 
 cr_loaded_list = json.loads(cr_json)
 
@@ -19,3 +21,17 @@ cr_loaded_list = json.loads(cr_json)
 # do something with the cr_loaded_list
 
 print(cr_loaded_list[25]['raw_text'])
+
+
+#%%
+# Let's see how it looks if we load the data in as a pandas dataframe
+df = pd.read_json(cr_json_filepath)
+
+# Put the date as a column in our dataframe
+split_date = dateString.split('.')[0].split('-')
+df['session'] = split_date[1] + '-' + split_date[2] + '-' + split_date[3]
+
+print(df.head())
+print("\n==================================\n")
+print(df.iloc[25, 2])
+
